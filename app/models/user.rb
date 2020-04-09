0127747e4c9674c16e420f55076021a8e30105c6
Matthew Kangvanvongsa <matthewmetaveemomo@Matthews-MacBook-Pro.local>
2020-04-09 18:09:17 -0500

    try to push to heruku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  if(ENV['DATABASE_URL'])
+     uri = URI.parse(ENV['DATABASE_URL'])
+     DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+ else
+     DB = PG.connect(host: "localhost", port: 5432, dbname: 'simplerails')
+ end
 end
