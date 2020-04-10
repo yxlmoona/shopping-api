019bd59fca4b5720a47955e9fa04dbe403026303
Matthew Kangvanvongsa <matthewmetaveemomo@Matthews-MacBook-Pro.local>
2020-04-10 11:35:04 -0500

    change loves to love

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,10 +1,10 @@
 class Brand < ApplicationRecord
-  if(ENV['DATABASE_URL'])
-     uri = URI.parse(ENV['DATABASE_URL'])
-     DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
- else
-     DB = PG.connect(host: "localhost", port: 5432, dbname: 'simplerails')
- end
+ #  if(ENV['DATABASE_URL'])
+ #     uri = URI.parse(ENV['DATABASE_URL'])
+ #     DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+ # else
+ #     DB = PG.connect(host: "localhost", port: 5432, dbname: 'simplerails')
+ # end
   has_many :items
   # has_many :reviews
 end
